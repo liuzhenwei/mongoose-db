d8bc03115d1abf725535a012bb075aaa7ad9cd04
Will <will.liu@nakedhub.cn>
2017-06-13 10:44:23 +0800

    1.0.3

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const objectAssign = require('object-assign');
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
@@ -9,10 +8,10 @@ module.exports = (function(){
 		this.model = this.conn.model(name, new mongoose.Schema(schema));
 	}
 
-	Model.prototype = objectAssign(Model.prototype, {
+	Model.prototype = {
 		get: function(query, options, callback){
-			var Model = this.model;
-			var Query = null;
+			let Model = this.model;
+			let Query = null;
 
 			if( typeof(query) == 'function' ){
 				callback = query;
@@ -60,7 +59,7 @@ module.exports = (function(){
 		},
 
 		set: function(query, data, callback){
-			var Model = this.model;
+			let Model = this.model;
 
 			callback = callback || function(){};
 
@@ -68,12 +67,10 @@ module.exports = (function(){
 				if (error) return callback(error, null);
 
 				if (result && result.length > 0) {
-					Model.update(query, {$set: data}, updateErr => {
+					Model.update(query, {$set: data}, (updateErr) => {
 						if (updateErr) return callback(updateErr, null);
-						result.forEach(function(item, i){
-							item = objectAssign(item, data);
-						});
-						callback(null, result);
+						
+						this.get({_id: result[0]._id}, callback);
 					});
 				} else {
 					callback({message: 'not found'}, null);
@@ -82,19 +79,21 @@ module.exports = (function(){
 		},
 
 		setById: function(id, data, callback){
-			var Model = this.model;
+			let Model = this.model;
 
 			callback = callback || function(){};
 
 			Model.findByIdAndUpdate(id, {$set: data}, (error, result) => {
 				if (error) return callback(error, null);
-				callback(null, objectAssign(result, data));
+				
+				this.get({_id: id}, callback);
 			});
 		},
 
 		update: function(query, update, options, callback){
-			var Model = this.model;
+			let Model = this.model;
 
+			options = options || {};
 			callback = callback || function(){};
 			if( typeof(options) == 'function' ){
 				callback = options;
@@ -117,8 +116,9 @@ module.exports = (function(){
 		},
 
 		updateById: function(id, update, options, callback){
-			var Model = this.model;
+			let Model = this.model;
 
+			options = options || {};
 			callback = callback || function(){};
 			if( typeof(options) == 'function' ){
 				callback = options;
@@ -133,7 +133,7 @@ module.exports = (function(){
 		},
 
 		delete: function(query, callback){
-			var Model = this.model;
+			let Model = this.model;
 
 			callback = callback || function(){};
 
@@ -141,7 +141,7 @@ module.exports = (function(){
 				if (error) return callback(error, null);
 
 				if (result && result.length > 0) {
-					Model.remove(query, deleteErr => {
+					Model.remove(query, (deleteErr) => {
 						if (deleteErr) return callback(deleteErr, null);
 						callback(null, result);
 					});
@@ -152,7 +152,7 @@ module.exports = (function(){
 		},
 
 		deleteById: function(id, callback){
-			var Model = this.model;
+			let Model = this.model;
 
 			callback = callback || function(){};
 
@@ -163,7 +163,7 @@ module.exports = (function(){
 		},
 
 		create: function(data, callback){
-			var Model = this.model;
+			let Model = this.model;
 
 			callback = callback || function(){};
 
@@ -172,66 +172,64 @@ module.exports = (function(){
 				callback(null, result);
 			});
 		}
-	});
+	};
 
-	function Connection(name, openArgs){
+	function Connection(name, ...args){
 		this.name = name;
 		this.conn = mongoose.createConnection();
 		this.connected = false;
 
-		if( openArgs.length > 0 ){
-			this.open.apply(this, openArgs);
+		if( args.length > 0 ){
+			this.open(...args);
 		}
 	}
 
-	Connection.prototype = objectAssign(Connection.prototype, {
-		open: function(){
-			var args = Array.prototype.slice.call(arguments);
-			this.conn.open.apply(this.conn, args);
-			this.conn.once('connected', function(){
+	Connection.prototype = {
+		open: function(...args){
+			this.conn.open(...args);
+			this.conn.once('connected', () => {
 				this.connectUri = args[0];
 				this.connected = true;
-			}.bind(this));
+			});
 		},
 
 		close: function(callback){
-			this.conn.close(function(){
+			this.conn.close(() => {
 				this.connected = false;
 				callback && callback();
-			}.bind(this));
+			});
 		},
 
 		setModels: function(models){
-			for( var name in models ){
+			for( let name in models ){
 				this[name] = new Model(name, models[name], this.conn);
 			}
 		}
-	});
+	};
 
 	return {
 		_connections: {},
 
-		createConnection: function(name){
-			var args = Array.prototype.slice.call(arguments, 1);
+		createConnection: function(name, ...args){
 			if( this._connections[name] ){
-				var connection = this._connections[name];
+				let connection = this._connections[name];
 				if( connection.connected == true ){
 					connection.close(function(){
 						connection.connected = false;
-						connection.open.apply(connection, args);
+						connection.open(...args);
 					});
 				} else {
-					connection.open.apply(connection, args);
+					connection.open(...args);
 				}
 			} else {
-				this._connections[name] = new Connection(name, args);
+				this._connections[name] = new Connection(name, ...args);
 				this[name] = this._connections[name];
 			}
 		},
 
 		closeConnection: function(name, callback){
 			if( name && this._connections[name] ){
-				var connection = this._connections[name];
+				let connection = this._connections[name];
 				connection.close(function(){
 					connection.connected = false;
 					callback && callback();
